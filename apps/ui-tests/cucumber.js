de3671a588fb5abe0c36d20cf6abe30f19ec3bf7
Tony <stutlerxgod@gmail.com>
2026-03-30 18:56:36 +0200

    fix: change cucumber report path

--- apps/ui-tests/cucumber.js
+++ apps/ui-tests/cucumber.js
@@ -6,7 +6,7 @@ module.exports = {
       'apps/ui-tests/steps/**/*.ts',
     ],
     paths: ['apps/ui-tests/features/**/*.feature'],
-    format: ['progress', 'html:apps/ui-tests/reports/cucumber/cucumber-report.html'],
+    format: ['progress', 'html:apps/ui-tests/reports/cucumber-report.html'],
     parallel: 4,
     worldParameters: {
       headless: true,
